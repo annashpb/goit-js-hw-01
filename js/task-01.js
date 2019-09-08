a227d43d94639ed8d60f1c4a88bf3401da2cd792
Anna Sh <anna.sh.pb@gmail.com>
2019-09-08 21:18:40 +0300

    task-01

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const name = 'Генератор защитного поля';
+let price = 1000;
+
+let userChoise = `Выбран «${name}», цена за штуку ${price} кредитов`;
+console.log(userChoise);
+
+price = 2000;
+userChoise = `Выбран «${name}», цена за штуку ${price} кредитов`;
+console.log(userChoise);

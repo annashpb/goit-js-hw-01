aad5618adba3a9dc06234e3ce2e93fa754c77bcf
Anna Sh <anna.sh.pb@gmail.com>
2019-09-08 21:29:54 +0300

    task-02

---
+++ js/task-02.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const total = 100;
+const ordered = 50;
+
+const stockBalanseEnoagh = ordered > total ? 'На складе недостаточно товаров!' : 'Заказ оформлен, с вами свяжется менеджер';
+console.log(stockBalanseEnoagh);
